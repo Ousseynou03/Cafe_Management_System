f220bc698705b6b50f15f8930a9cd03bead0438f
Dione <dioneousseynou03@gmail.com>
2023-02-15 18:23:04 +0100

    signup User & Test API

--- src/main/java/com/dione/cafe_management_sysem/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/dione/cafe_management_sysem/serviceImpl/UserServiceImpl.java
@@ -24,13 +24,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public ResponseEntity<String> signUp(Map<String, String> requestMap) {
         log.info("Inside signup {}", requestMap);
+        try {
+
+
         if (valudateSignUpMap(requestMap)){
             User user = userDao.findByEmailId(requestMap.get("email"));
    /*         On va établir une condition comme quoi que si l'email n'est pas trouvé
             l'object user sera quant à lui null
+            Et dans ce cas on pourra créer un nouveau user sinon c'est parce que l' email existe déjà.
             */
             if (Objects.isNull(user)){
-
+                userDao.save(getUserFromMap(requestMap));
+                return CafeUtils.getResponseEntity("Successful Registered", HttpStatus.OK);
             }else {
                 //Vérification si le mail existe déjà.
                 return CafeUtils.getResponseEntity("Emil already exits.", HttpStatus.BAD_REQUEST);
@@ -38,7 +43,10 @@ public class UserServiceImpl implements UserService {
         }else {
             return CafeUtils.getResponseEntity(CafeConstants.INVALID_DATA, HttpStatus.BAD_REQUEST);
         }
-        return null;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return CafeUtils.getResponseEntity(CafeConstants.SOMETHING_WEBT_WRONG, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private boolean valudateSignUpMap(Map<String, String> requestMap){
@@ -49,5 +57,16 @@ public class UserServiceImpl implements UserService {
         else {
             return false;
         }
+    }
+
+    //Création d'une classe qui va extraire la valeur de requestMap
+    private User getUserFromMap(Map<String, String> requestMap){
+        User user = new User();
+        user.setName(requestMap.get("name"));
+        user.setEmail(requestMap.get("email"));
+        user.setContactNumber(requestMap.get("contactNumber"));
+        user.setPassword(requestMap.get("password"));
+        user.setRole(requestMap.get("role"));
+        return user;
     }
 }
